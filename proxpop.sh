4c26f2cdf90db065a754ccf5b0c4068c0a53cb87
verghost <23479546+verghost@users.noreply.github.com>
2020-12-06 08:27:17 -0500

    added chains, file formats (still not done)
    very much needs testing and more complete features
    also better way of doing file reads

--- proxpop.sh
+++ proxpop.sh
@@ -15,12 +15,15 @@ usage: proxpop.sh [options]
 	
 	[Template Options]
 	-t, --template Use custom template file (Default is proxychains.template)
+	-c, --chain    Specify a custom chain file (ex. proxpop.sh ... -c chain.txt)
+	-r, --resource Specify a custom resource file (ex. proxpop.sh ... -r resource.txt)
 	-q, --quiet    Use proxychains quiet mode
 	--no-proxy-dns Do NOT proxy DNS requests (this is ON by default)
 	--use-strict   Use strict chain in template file (default is dynamic_chain)
 	--use-random   Use random chain. You can optionally provide chain length: proxpop.sh ... --use-random 3 (Default length is: 2)
 	
 	[Saftey Options]
+	-o, --output   Choose an output location for the new config file (Default is /etc/proxychains.conf)
 	-k, --keep     Keep old configuration file (Old file is copied to /usr/share/proxpop/proxychains.old)
 	--restore      Restore the old configuration file (this does not run proxpop)
 	--review       Review final configuration file before finalizing (--silent does not apply to this option)
@@ -30,7 +33,6 @@ usage: proxpop.sh [options]
 	--proxy-fetch  Try to fetch resources through proxy (may not work if proxychains is improperly configured)
 	
 	[Other Options]
-	-r, --resource Specify a custom resource file (ex. proxpop.sh ... -r resource.txt)
 	-s, --silent   Do not display output (does not apply to --review, torify or proxychains)
 	-h, --help     Show this message
 
@@ -62,18 +64,27 @@ PP_TOPTS["tcp_connect_time_out"]=8000
 TMP_HTTP="/tmp/http_prox"
 TMP_SOCKS4="/tmp/socks5_prox"
 TMP_SOCKS5="/tmp/socks4_prox"
-TMP_CONF="/tmp/proxychains.conf"
+TMP_CONF="/tmp/proxychains_${RANDOM}.conf"
 
-PC_CONF_PATH="/etc/proxychains.conf"
+PROXPOP_OUTPUT="/etc/proxychains.conf"
 OLD_CONFIG="/usr/share/proxpop/proxychains.old"
 
-PP_CURL="curl" # curl function
-
+CHAIN_FILE=""
 RESOURCE_FILE="/usr/share/proxpop/resources.default" # Defaults
 TEMPLATE_FILE="/usr/share/proxpop/proxychain.template"
 
 
 # Util functions
+pp_yorn() {
+	PP_YORN_RET=""
+	while true; do
+		echo -n "$1 (y/N): "
+		read yorn
+		if [[ "$yorn" == "y" ]] || [[ "$yorn" == "Y" ]]; then PP_YORN_RET="y"; break
+		elif [[ "$yorn" == "n" ]] || [[ "$yorn" == "N" ]]; then PP_YORN_RET="n"; break
+		else echo "Not a valid response!"; fi
+	done
+}
 
 pp_echo() {
 	if [[ ! $PROXPOP_SILENT ]]; then echo "$*"; fi
@@ -98,12 +109,11 @@ pp_exit() {
 }
 
 pp_curl() {
-	local out=$1; shift;
-	
+	local out=$1; shift
 	if [[ $PROXPOP_TORFETCH ]]; then 
-		torify curl -s -o $out "$*"
+		torify curl -s -o $out $*
 	elif [[ $PROXPOP_PROXYFETCH ]]; then
-		proxychains curl -s -o $out "$*"
+		proxychains curl -s -o $out $*
 	else
 		curl -s -o $out $*
 	fi
@@ -120,19 +130,21 @@ add_proxy() {
 
 fetch_proxies() {
 	pp_echo "Fetching proxies..."
-	echo "[ProxyList]" >> $TMP_CONF
 	
 	#[[ $PROXPOP_TORFIRST ]] && echo "$TEMPLATE_TORLINE" >> $TMP_CONF
 	
 	while IFS= read -r rec; do
 		if [[ "${rec:0:4}" == "http" ]] && [[ ! "$PROXPOP_HTTP" == "" ]]; then
 			pp_curl $TMP_HTTP "${rec:5}"
+			[[ ! "$PROXPOP_HTTP" == "all" ]] && cat $TMP_HTTP | head -n $PROXPOP_HTTP > $TMP_HTTP # if we need to correct how many proxies should be added
 			add_proxy "http" $TMP_HTTP
 		elif [[ "${rec:0:6}" == "socks4" ]] && [[ ! "$PROXPOP_SOCKS4" == "" ]]; then
 			pp_curl $TMP_SOCKS4 "${rec:6}"
+			[[ ! "$PROXPOP_SOCKS4" == "all" ]] && cat $TMP_SOCKS4 | head -n $PROXPOP_SOCKS4 > $TMP_SOCKS4
 			add_proxy "socks4" $TMP_SOCKS4
 		elif [[ "${rec:0:6}" == "socks5" ]] && [[ ! "$PROXPOP_SOCKS5" == "" ]]; then
 			pp_curl $TMP_SOCKS5 "${rec:6}"
+			[[ ! "$PROXPOP_SOCKS5" == "all" ]] && cat $TMP_SOCKS5 | head -n $PROXPOP_SOCKS5 > $TMP_SOCKS5
 			add_proxy "socks5" $TMP_SOCKS5
 		fi
 	done < $RESOURCE_FILE
@@ -142,10 +154,10 @@ populate() {
 	pp_echo ""
 	pp_echo "Starting ProxPop..."
 	
-	[[ $PROXPOP_KEEP ]] && cp -f $PC_CONF_PATH $OLD_CONFIG # keep old config 
+	[[ $PROXPOP_KEEP ]] && cp -f $PROXPOP_OUTPUT $OLD_CONFIG # keep old config 
 	
 	pp_echo "Writing configuration file..."
-	touch $TMP_CONF # create base config file
+	echo "" > $TMP_CONF # create empty config file
 	echo $TEMPLATE_MODE > $TMP_CONF
 	for k in "${!PP_TOPTS[@]}"; do
 		if [[ ! "${PP_TOPTS[$k]:0:1}" == "#" ]]; then
@@ -153,9 +165,10 @@ populate() {
 		fi
 	done
 	
+	echo "[ProxyList]" >> $TMP_CONF # start proxy list in config
 	fetch_proxies
 	
-	chmod --reference=$PC_CONF_PATH $TMP_CONF
+	chmod --reference=$PROXPOP_OUTPUT $TMP_CONF
 	
 	sleep 1
 	pp_echo "New configuration is ready!"
@@ -167,23 +180,17 @@ populate() {
 
 		more $TMP_CONF
 		echo ""
-
-		while true; do
-			echo -n "If this OK? (y/N): "
-			read yorn
-			if [[ "$yorn" == "y" ]] || [[ "$yorn" == "Y" ]]; then
-				echo "Replacing current config..."
-				mv -f $TMP_CONF $PC_CONF_PATH
-				break
-			elif [[ "$yorn" == "n" ]] || [[ "$yorn" == "N" ]]; then
-				echo "OK, aborting..."
-				pp_exit 0
-			else
-				echo "Not a valid response!"
-			fi
-		done
+		
+		pp_yorn "If this OK?" # start yorn
+		if [[ "$PP_YORN_RET" == "y" ]]; then
+			echo "Replacing current config..."
+			mv -f $TMP_CONF $PROXPOP_OUTPUT
+		else
+			echo "OK, aborting..."
+			pp_exit 0
+		fi
 	else
-		mv -f $TMP_CONF $PC_CONF_PATH
+		mv -f $TMP_CONF $PROXPOP_OUTPUT
 	fi
 	
 	pp_exit 0
@@ -197,6 +204,15 @@ set_template_mode() {
 	fi
 }
 
+get_file() {
+	if [[ -f $1 ]]; then 
+		echo $1
+		shift
+	else 
+		pp_error "$1 is not a file!"
+	fi
+}
+
 get_first_n() {
 	local ret="$2" # default value
 	case "$1" in # https://stackoverflow.com/a/3951175
@@ -205,12 +221,6 @@ get_first_n() {
 	esac
 	echo "$ret"
 }
-
-if [ "$EUID" -ne 0 ]; then
-	pp_error "Error: This script must be run with elevated privileges!"
-elif [[ $(command -v proxychains) ]]; then
-	pp_error "Error: You must have proxychains installed!"
-fi
 
 while [[ "$#" -gt 0 ]]; do
 	case "$1" in
@@ -239,15 +249,9 @@ while [[ "$#" -gt 0 ]]; do
 	;;
 	
 	# Template options
-	-t|--template)
-		shift
-		if [[ -f $1 ]]; then
-			TEMPLATE_FILE=$1
-			shift
-		else
-			pp_error "$1 is not a file!"
-		fi
-	;;
+	-t|--template) shift; TEMPLATE_FILE=$(get_file $1) ;;
+	-r|--resource) shift; RESOURCE_FILE=$(get_file $1) ;;
+	-c|--chain)    shift; CHAIN_FILE=$(get_file $1) ;;
 	-q|--quiet) PP_TOPTS["quiet_mode"]=""; shift; ;;
 	--no-proxy-dns) PP_TOPTS["proxy_dns"]="#"; shift; ;;
 	--use-strict) set_template_mode "strict_chain"; shift; ;;
@@ -261,10 +265,22 @@ while [[ "$#" -gt 0 ]]; do
 	;;
 	
 	# Safety options
+	-o|--output)
+		shift
+		if 
+		if [[ -f $1 ]]; then
+			yorn "Wait! There is already a file at $1: do you want to overwrite it?"
+			if [[ "$PP_YORN_RET" == "n" ]]; then
+				echo "OK, aborting..."
+				pp_exit 0
+			fi
+		fi
+		PROXPOP_OUTPUT=$1; shift
+	;;
 	-k|--keep) PROXPOP_KEEP=1; shift; ;;
 	--restore)
 		if [[ -f $OLD_CONFIG ]]; then
-			cp -f $OLD_CONFIG $PC_CONF_PATH
+			cp -f $OLD_CONFIG $PROXPOP_OUTPUT
 			echo "Old configuration file was restored!"
 			pp_exit 0
 		else
@@ -285,15 +301,6 @@ while [[ "$#" -gt 0 ]]; do
 	--proxy-fetch) PROXPOP_PROXYFETCH=1; shift; ;;
 	
 	# Other Options
-	-r|--resource)
-		shift
-		if [[ -f $1 ]]; then
-			RESOURCE_FILE=$1
-			shift
-		else
-			pp_error "$1 is not a file!"
-		fi
-	;;
 	-s|--silent) PROXPOP_SILENT=1; shift; ;;
 	-h|--help) echo "$usage"; exit; ;;
 	
@@ -307,8 +314,17 @@ while [[ "$#" -gt 0 ]]; do
 	esac
 done
 
+# Check for dependencies
+if [ "$EUID" -ne 0 ]; then
+	pp_error "Error: This script must be run with elevated privileges!"
+elif [[ ! $(command -v curl) ]]; then
+	pp_error "Error: You must have curl installed!"
+elif [[ ! $(command -v proxychains) ]]; then
+	pp_error "Error: You must have proxychains installed!"
+fi
+
 # Run if a proxy type has been specified
-if [[ $PROXPOP_HTTP ]] || [[ $PROXPOP_SOCKS4 ]] || [[ $PROXPOP_SOCKS5 ]]; then
+if [[ ! "$PROXPOP_HTTP" == "" ]] || [[ ! "$PROXPOP_SOCKS4" == "" ]] || [[ ! "$PROXPOP_SOCKS5" == "" ]]; then
 	populate
 else
 	echo "You must supply at least one type of proxy to populate (--http, --socks4, --socks5, --all)"

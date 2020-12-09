67758191205d4f5409f0ca2d8db86feef96ed92d
verghost <23479546+verghost@users.noreply.github.com>
2020-12-09 04:40:33 -0500

    fixes (thanks shellcheck)
    still not done!

--- proxpop.sh
+++ proxpop.sh
@@ -7,11 +7,10 @@ usage=\
 A simple bash script that populates the proxychains config (/etc/proxychains.conf) with proxies from custom resources.
 
 usage: proxpop.sh [options]
-	[Proxy Types] For each type, you can chosse to only fetch the first n proxies (ex. proxpop.sh --http 4).
-	--http   [n]   Populate HTTP proxies
-	--socks4 [n]   Populate SOCKS4 proxies
-	--scosk5 [n]   Populate SCOKS5 proxies
-	--all    [n]   Populate all types of proxies (HTTP, SOCKS4 & SOCKS5)
+	[Proxy Types]
+	--only-http    Populate only HTTP proxies
+	--only-socks4  Populate only SOCKS4 proxies
+	--only-socks5  Populate only SCOKS5 proxies
 	
 	[Template Options]
 	-t, --template Use custom template file (Default is proxychains.template)
@@ -35,24 +34,10 @@ usage: proxpop.sh [options]
 	[Other Options]
 	-s, --silent   Do not display output (does not apply to --review, torify or proxychains)
 	-h, --help     Show this message
-
-NOTES:
-By default, some free proxies from the ProxyScrap API are used.
-To specify custom locations a FILE can be supplied to the script. This overrides the default locations.
-
-The custom resource FILE must specify resources one at a time, each on their own line. 
-Each resource lines should have the following format TYPE:URL where TYPE is one of HTTP, SOCKS4, SOCKS5 (This is CASE SENSITIVE)
-As an example, see the defaults resource file:
-
-Each resource should return a list of proxy IPs and PORT in the IP:PORT formate, for example:
-103.31.251.17:8080
-37.255.248.77:8080
-221.182.31.54:8080
-106.14.237.164:8080
 '
 
 TEMPLATE_MODE="dynamic_chain"
-TEMPLATE_TORLINE="socks4 127.0.0.1 9050"
+#TEMPLATE_TORLINE="socks4 127.0.0.1 9050"
 
 declare -A PP_TOPTS # template options
 PP_TOPTS["quiet_mode"]="#" # this indicates that we will comment out this option
@@ -60,6 +45,8 @@ PP_TOPTS["proxy_dns"]=""
 PP_TOPTS["chain_len"]="#"
 PP_TOPTS["tcp_read_time_out"]=15000
 PP_TOPTS["tcp_connect_time_out"]=8000
+
+PROXPOP_HTTP=1; PROXPOP_SOCKS4=1; PROXPOP_SOCKS5=1;
 
 TMP_HTTP="/tmp/http_prox"
 TMP_SOCKS4="/tmp/socks5_prox"
@@ -76,14 +63,21 @@ TEMPLATE_FILE="/usr/share/proxpop/proxychain.template"
 
 # Util functions
 pp_yorn() {
-	PP_YORN_RET=""
 	while true; do
 		echo -n "$1 (y/N): "
-		read yorn
+		read -r yorn
 		if [[ "$yorn" == "y" ]] || [[ "$yorn" == "Y" ]]; then PP_YORN_RET="y"; break
 		elif [[ "$yorn" == "n" ]] || [[ "$yorn" == "N" ]]; then PP_YORN_RET="n"; break
 		else echo "Not a valid response!"; fi
 	done
+}
+
+# Check for natural number (defined for our purposes as an integer >=0)
+pp_isnat() {
+	case "$1" in # https://stackoverflow.com/a/3951175
+		''|*[!0-9]*) ;; # negates strings, floating point and negative numbers
+		*) echo 1 ;;
+	esac
 }
 
 pp_echo() {
@@ -93,7 +87,7 @@ pp_echo() {
 pp_error() {
 	local code=$2; [[ "$code" == "" ]] && code=1;
 	echo "$1" >&2
-	exit $code
+	exit "$code"
 }
 
 pp_exit() {
@@ -105,49 +99,73 @@ pp_exit() {
 	echo "| ALL DONE |"
 	echo "| -------- |"
 	
-	exit $1
+	exit "$1"
 }
 
 pp_curl() {
 	local out=$1; shift
+	local tmpf="./.tmpfile_${RANDOM}"
 	if [[ $PROXPOP_TORFETCH ]]; then 
-		torify curl -s -o $out $*
+		torify curl -s -o $tmpf "$@"
 	elif [[ $PROXPOP_PROXYFETCH ]]; then
-		proxychains curl -s -o $out $*
+		proxychains curl -s -o $tmpf "$@"
 	else
-		curl -s -o $out $*
+		curl -s -o $tmpf "$@"
 	fi
+	cat $tmpf >> "$out"
+	rm -f $tmpf
 }
+
+set_template_mode() {
+	# is this still the default?
+	if [[ "$TEMPLATE_MODE" == "dynamic_chain" ]]; then TEMPLATE_MODE="$1"
+	else pp_error "You must only specify one mode!"; fi
+}
+
+get_file() {
+	if [[ -f $1 ]]; then echo "$1"; shift
+	else pp_error "$1 is not a file!"; fi
+}
+
+get_first_n() {
+	if [[ $(pp_isnat "$1") ]]; then echo "$1"
+	else echo "$2"; fi # default value
+}
+
+# ProxPop main code
 
 add_proxy() {
-	local type=$1
-	local file=$2
+	local type=$1 file=$2 n=$3 i=0
 	while IFS= read -r ipp; do
+		if [[ ! "$n" == "" ]] && [[ $i -ge $n ]]; then break; fi
 		echo "$type $ipp" | awk -F':' '{print $1,$2}' >> $TMP_CONF
-	done < $file
-	rm -f $file # clear temp file
+		((i++))
+	done < "$file"
+	pp_echo "Added $i '${type}' proxies!"
 }
 
-fetch_proxies() {
-	pp_echo "Fetching proxies..."
-	
-	#[[ $PROXPOP_TORFIRST ]] && echo "$TEMPLATE_TORLINE" >> $TMP_CONF
-	
-	while IFS= read -r rec; do
-		if [[ "${rec:0:4}" == "http" ]] && [[ ! "$PROXPOP_HTTP" == "" ]]; then
-			pp_curl $TMP_HTTP "${rec:5}"
-			[[ ! "$PROXPOP_HTTP" == "all" ]] && cat $TMP_HTTP | head -n $PROXPOP_HTTP > $TMP_HTTP # if we need to correct how many proxies should be added
-			add_proxy "http" $TMP_HTTP
-		elif [[ "${rec:0:6}" == "socks4" ]] && [[ ! "$PROXPOP_SOCKS4" == "" ]]; then
-			pp_curl $TMP_SOCKS4 "${rec:6}"
-			[[ ! "$PROXPOP_SOCKS4" == "all" ]] && cat $TMP_SOCKS4 | head -n $PROXPOP_SOCKS4 > $TMP_SOCKS4
-			add_proxy "socks4" $TMP_SOCKS4
-		elif [[ "${rec:0:6}" == "socks5" ]] && [[ ! "$PROXPOP_SOCKS5" == "" ]]; then
-			pp_curl $TMP_SOCKS5 "${rec:6}"
-			[[ ! "$PROXPOP_SOCKS5" == "all" ]] && cat $TMP_SOCKS5 | head -n $PROXPOP_SOCKS5 > $TMP_SOCKS5
-			add_proxy "socks5" $TMP_SOCKS5
-		fi
-	done < $RESOURCE_FILE
+add_proxies() {
+	local type="" file="" rol=""
+	if [[ -f $CHAIN_FILE ]]; then
+		pp_echo "Parsing chain file..."
+		while IFS= read -r line; do
+			if [[ "${line:0:4}" == "http" ]]; then type="http"; file=$TMP_HTTP; rol="${line:5}"
+			elif [[ "${line:0:6}" == "socks4" ]]; then type="socks4"; file=$TMP_SOCKS4; rol="${line:7}"
+			elif [[ "${line:0:6}" == "socks5" ]]; then type="socks5"; file=$TMP_SOCKS5; rol="${line:7}"
+			else continue; fi
+			
+			if [[ "$rol" == "rand" ]]; then
+				pp_error "Not done yet!"
+			elif [[ ! "$rol" == "" ]]; then
+				if [[ $(pp_isnat "$rol") ]]; then add_proxy $type $file "$rol"
+				else pp_error "Bad line in chain file: $line"; fi
+			else add_proxy $type $file; fi
+		done < "$CHAIN_FILE"
+	else
+		[[ $PROXPOP_HTTP ]] && add_proxy "http" $TMP_HTTP
+		[[ $PROXPOP_SOCKS4 ]] && add_proxy "socks4" $TMP_SOCKS4
+		[[ $PROXPOP_SOCKS5 ]] && add_proxy "socks5" $TMP_SOCKS5
+	fi
 }
 
 populate() {
@@ -157,108 +175,82 @@ populate() {
 	[[ $PROXPOP_KEEP ]] && cp -f $PROXPOP_OUTPUT $OLD_CONFIG # keep old config 
 	
 	pp_echo "Writing configuration file..."
-	echo "" > $TMP_CONF # create empty config file
-	echo $TEMPLATE_MODE > $TMP_CONF
-	for k in "${!PP_TOPTS[@]}"; do
-		if [[ ! "${PP_TOPTS[$k]:0:1}" == "#" ]]; then
-			echo "$k ${PP_TOPTS[$k]}" >> $TMP_CONF
-		fi
-	done
+	if [[ -f $TEMPLATE_FILE ]]; then
+		cp -f $TEMPLATE_FILE $TMP_CONF
+	else
+		echo "" > $TMP_CONF # create empty config file
+		echo $TEMPLATE_MODE > $TMP_CONF
+		for k in "${!PP_TOPTS[@]}"; do
+			if [[ ! "${PP_TOPTS[$k]:0:1}" == "#" ]]; then
+				echo "$k ${PP_TOPTS[$k]}" >> $TMP_CONF
+			fi
+		done
+	fi
 	
 	echo "[ProxyList]" >> $TMP_CONF # start proxy list in config
-	fetch_proxies
 	
-	chmod --reference=$PROXPOP_OUTPUT $TMP_CONF
+	pp_echo "Fetching proxies..."
+	while IFS= read -r rec; do
+		if [[ "${rec:0:4}" == "http" ]] && [[ $PROXPOP_HTTP ]]; then
+			pp_curl $TMP_HTTP "${rec:5}"
+		elif [[ "${rec:0:6}" == "socks4" ]] && [[ $PROXPOP_SOCKS4 ]]; then
+			pp_curl $TMP_SOCKS4 "${rec:6}"
+		elif [[ "${rec:0:6}" == "socks5" ]] && [[ $PROXPOP_SOCKS5 ]]; then
+			pp_curl $TMP_SOCKS5 "${rec:6}"
+		fi
+	done < "$RESOURCE_FILE"
+	
+	add_proxies
 	
 	sleep 1
 	pp_echo "New configuration is ready!"
 	
 	if [[ $PROXPOP_REVIEW ]]; then
-		sleep 1
-		echo "Please review the new file when it appears..."
-		sleep 2
-
-		more $TMP_CONF
-		echo ""
-		
+		sleep 1; echo "Please review the new file when it appears..."; sleep 2; more $TMP_CONF; echo ""
 		pp_yorn "If this OK?" # start yorn
-		if [[ "$PP_YORN_RET" == "y" ]]; then
-			echo "Replacing current config..."
-			mv -f $TMP_CONF $PROXPOP_OUTPUT
-		else
-			echo "OK, aborting..."
-			pp_exit 0
-		fi
-	else
-		mv -f $TMP_CONF $PROXPOP_OUTPUT
+		if [[ "$PP_YORN_RET" == "y" ]]; then echo "OK, we got this!"
+		else echo "OK, aborting..."; pp_exit 0; fi
 	fi
+	
+	pp_echo "Writing final configuration..."
+	chmod --reference=$PROXPOP_OUTPUT $TMP_CONF
+	mv -f $TMP_CONF $PROXPOP_OUTPUT
 	
 	pp_exit 0
 }
 
-set_template_mode() {
-	if [[ "$TEMPLATE_MODE" == "dynamic_chain" ]]; then # is this still the default?
-		TEMPLATE_MODE="$1"
-	else
-		pp_error "You must only specify one mode!"
-	fi
-}
-
-get_file() {
-	if [[ -f $1 ]]; then 
-		echo $1
-		shift
-	else 
-		pp_error "$1 is not a file!"
-	fi
-}
-
-get_first_n() {
-	local ret="$2" # default value
-	case "$1" in # https://stackoverflow.com/a/3951175
-		''|*[!0-9]*) ;; # negates strings, floating point and negative numbers
-		*) ret=$1 ;;
-	esac
-	echo "$ret"
-}
-
+# Parse params
+#declare -A PARAM_HISTORY
 while [[ "$#" -gt 0 ]]; do
 	case "$1" in
 	# Proxy types
-	--http) 
+	--only-http) 
 		shift
-		PROXPOP_HTTP="$(get_first_n $1 all)"
-		[[ ! "$PROXPOP_HTTP" == "all" ]] && shift
+		PROXPOP_SOCKS4=; PROXPOP_SOCKS5=
 	;;
-	--socks4) 
+	--only-socks4) 
 		shift
-		PROXPOP_SOCKS4="$(get_first_n $1 all)"
-		[[ ! "$PROXPOP_SOCKS4" == "all" ]] && shift
+		PROXPOP_HTTP=; PROXPOP_SOCKS5=
 	;;
-	--socks5)
+	--only-socks5) 
 		shift
-		PROXPOP_SOCKS5="$(get_first_n $1 all)"
-		[[ ! "$PROXPOP_SOCKS5" == "all" ]] && shift
-	;;
-	--all)
-		shift
-		PROXPOP_HTTP="$(get_first_n $1 all)"
-		PROXPOP_SOCKS4="$PROXPOP_HTTP"
-		PROXPOP_SOCKS5="$PROXPOP_HTTP"
-		[[ ! "$PROXPOP_HTTP" == "all" ]] && shift
+		PROXPOP_HTTP=; PROXPOP_SOCKS4=
 	;;
 	
 	# Template options
-	-t|--template) shift; TEMPLATE_FILE=$(get_file $1) ;;
-	-r|--resource) shift; RESOURCE_FILE=$(get_file $1) ;;
-	-c|--chain)    shift; CHAIN_FILE=$(get_file $1) ;;
+	-t|--template) shift; TEMPLATE_FILE=$(get_file "$1") ;;
+	-r|--resource) shift; RESOURCE_FILE=$(get_file "$1") ;;
+	-c|--chain) 
+		shift
+		CHAIN_FILE=$(get_file "$1")
+	;;
 	-q|--quiet) PP_TOPTS["quiet_mode"]=""; shift; ;;
 	--no-proxy-dns) PP_TOPTS["proxy_dns"]="#"; shift; ;;
 	--use-strict) set_template_mode "strict_chain"; shift; ;;
 	--use-random)
 		set_template_mode "random_chain"
 		shift
-		PP_TOPTS["chain_len"]="= $(get_first_n $1 d)"
+		PP_TOPTS["chain_len"]="= $(get_first_n "$1" d)"
 		if [[ "${PP_TOPTS[chain_len]}" == "= d" ]]; then 
 			PP_TOPTS["chain_len"]="= 2"
 		else shift; fi
@@ -267,7 +259,6 @@ while [[ "$#" -gt 0 ]]; do
 	# Safety options
 	-o|--output)
 		shift
-		if 
 		if [[ -f $1 ]]; then
 			yorn "Wait! There is already a file at $1: do you want to overwrite it?"
 			if [[ "$PP_YORN_RET" == "n" ]]; then
@@ -280,7 +271,7 @@ while [[ "$#" -gt 0 ]]; do
 	-k|--keep) PROXPOP_KEEP=1; shift; ;;
 	--restore)
 		if [[ -f $OLD_CONFIG ]]; then
-			cp -f $OLD_CONFIG $PROXPOP_OUTPUT
+			cp -f $OLD_CONFIG "$PROXPOP_OUTPUT"
 			echo "Old configuration file was restored!"
 			pp_exit 0
 		else
@@ -314,7 +305,6 @@ while [[ "$#" -gt 0 ]]; do
 	esac
 done
 
-# Check for dependencies
 if [ "$EUID" -ne 0 ]; then
 	pp_error "Error: This script must be run with elevated privileges!"
 elif [[ ! $(command -v curl) ]]; then
